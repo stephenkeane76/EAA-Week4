96aeaed9b71c5f6abfdd6abe046cbf3d6e5d521d
stephenkeane76 <x00081541@ittd.ie>
2014-10-10 11:55:27 +0100

    test

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prompt$ ./script1.sh <$1, $2, $3, $4, $5,>
+./script1.sh <$1, $2, $3, $4, $5,>
 
-prompt$ ./script1.sh Stephen Keane
-prompt$ Hello Stephen
+./script1.sh Stephen Keane
+Hello Stephen
